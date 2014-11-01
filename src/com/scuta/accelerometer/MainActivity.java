30c61349cf9c4754af70ec4fd2c20a48833b6fe3
Yang <umsu2@myumanitoba.ca>
2014-10-31 20:59:17 -0500

    first commit

--- src/com/scuta/accelerometer/MainActivity.java
+++ src/com/scuta/accelerometer/MainActivity.java
@@ -9,12 +9,24 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
+
+
 public class MainActivity extends Activity implements SensorEventListener {
 
 	private SensorManager mSensorManager;
 	private Sensor mAccelerometer;
 	private TextView axisX, axisY, axisZ;
+	public TextView timenano,timearray; 
+	double timeStamp = 0;
+	double timeStampTemp=0;
+	
+	int i = 0;
+	double[][] samples = new double[10][4];
+	double[] onesample = new double[4];
+	
+	
 
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -23,6 +35,8 @@ public class MainActivity extends Activity implements SensorEventListener {
 		axisX = (TextView) findViewById(R.id.acc_x);
 		axisY = (TextView) findViewById(R.id.acc_y);
 		axisZ = (TextView) findViewById(R.id.acc_z);
+		timenano = (TextView) findViewById(R.id.timenano);
+		timearray = (TextView) findViewById(R.id.timearray);
 		
 		mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 		mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
@@ -36,14 +50,38 @@ public class MainActivity extends Activity implements SensorEventListener {
 
 	@Override
 	public void onSensorChanged(SensorEvent event) {
-		float x = event.values[0];
-	    float y = event.values[1];
-	    float z = event.values[2];
-	    
-	    axisX.setText("X: "+x);
-	    axisY.setText("Y: "+y);
-	    axisZ.setText("Z: "+z);
+		
+		TimeStamp timer1 = new TimeStamp();
 
+		
+//		float x = event.values[0];
+//		float y = event.values[1];
+//	    float z = event.values[2];
+//	    timeStamp = System.currentTimeMillis();
+//	    axisX.setText("X: "+x);
+//	    axisY.setText("Y: "+y);
+//	    axisZ.setText("Z: "+z);
+//	    timenano.setText("Time: "+ timeStamp);
+	    onesample[0] = event.values[0];
+	    onesample[1] = event.values[1];
+	    onesample[2] = event.values[2];   
+	    onesample[3] = timer1.returntime();
+	    samples[i] = onesample;
+	    
+	    
+	    axisX.setText("X: "+onesample[0]);
+	    axisY.setText("Y: "+onesample[1]);
+	    axisZ.setText("Z: "+onesample[2]);
+	    timenano.setText("Time: "+ samples[i][3]);
+	    double value = samples[1][3]-samples[5][3];
+	    
+	    timearray.setText("timearray: " + value);
+	    i++;
+	    if (i == 10)
+	    	{i = 0;}
+	    	
+	   
+	    
 	}
 	
 	@Override
---
+++ src/com/scuta/accelerometer/TimeStamp.java
@@ -0,0 +1,18 @@
+package com.scuta.accelerometer;
+
+public class TimeStamp {
+    
+    // the Bicycle class has
+    // three fields
+    public double time;
+        
+    // the Bicycle class has
+    // one constructor
+    public double returntime() {
+
+        
+    	return System.currentTimeMillis();
+        
+    }
+    }
+
