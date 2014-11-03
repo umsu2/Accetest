8cecfa5aedd5ee82ce1e097249b91435c9c9de62
Yang <umsu2@myumanitoba.ca>
2014-11-03 16:06:25 -0600

    added average to accelerometer, still have problem where the acceleromoeter speed changes unpredictably.

--- src/com/scuta/accelerometer/MainActivity.java
+++ src/com/scuta/accelerometer/MainActivity.java
@@ -1,6 +1,6 @@
 package com.scuta.accelerometer;
 
-import java.util.ArrayList;
+
 
 import android.app.Activity;
 import android.content.Context;
@@ -19,14 +19,10 @@ public class MainActivity extends Activity implements SensorEventListener {
 	private Sensor mAccelerometer;
 	private TextView axisX, axisY, axisZ;
 	public TextView timenano,timearray; 
-	//double timeStamp = 0;
-	//double timeStampTemp=0;
 	public dataSample[] array_10 = new dataSample[10];
 	int i = 0;
-//	int m = 0;
-//	public ArrayList array_10 = new ArrayList();
-	//double[][] samples = new double[10][4];
-	//double[] onesample = new double[4];
+	
+
 	
 	
 
@@ -43,32 +39,28 @@ public class MainActivity extends Activity implements SensorEventListener {
 		timearray = (TextView) findViewById(R.id.timearray);
 		
 		mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-		mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-
+	//	mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+	//	mSensorManager.registerListener(this, mAccelerometer, 100000);
+		mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),100000);
+		
 	}
 
 	@Override
 	public void onAccuracyChanged(Sensor sensor, int accuracy) {
-		 // Do something here if sensor accuracy changes.
+		//mSensorManager.registerListener(this, mAccelerometer, 100000); 
+		// Do something here if sensor accuracy changes.
 	}
 
 	@Override
 	public void onSensorChanged(SensorEvent event) {
-	// 	ArrayList<dataSample> array_10 = new ArrayList<dataSample>(10);
 		
+		
+		if(event.sensor.getType()==Sensor.TYPE_ACCELEROMETER){
 		TimeStamp timer_value = new TimeStamp();
 		dataSample first_data = new dataSample(event.values[0], event.values[1], event.values[2], timer_value.returntime());
 		array_10[i] = first_data;
-//		double[] onesample = new double[4];
-//		
-//
-//	    onesample[0] = event.values[0];
-//	    onesample[1] = event.values[1];
-//	    onesample[2] = event.values[2];   
-//	    onesample[3] = timer1.returntime();
-//	    samples[i] = onesample;
-//	    
-//	    
+		
+		
 	    axisX.setText("X: "+array_10[i].xaxis);
 	    axisY.setText("Y: "+array_10[i].yaxis);
 	    axisZ.setText("Z: "+array_10[i].zaxis);
@@ -79,17 +71,28 @@ public class MainActivity extends Activity implements SensorEventListener {
 	    i++;
 	    if (i == 10)
 	    	{
+	    	// insert algorithm here:
+	    	double average = 0; 
+	    	
+	    	for(int m = 0; m< 10;m++) 
+	    	{
+	    		average += array_10[m].xaxis;
+	    	}
+	    	
+	    	timearray.setText("x average: "+ average/10);
+	    	
+	    	//
 	    	i = 0;
 	    	}
 	    	
-	   
+		}
 	    
 	}
 	
 	@Override
 	  protected void onResume() {
 	    super.onResume();
-	    mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+	    mSensorManager.registerListener(this, mAccelerometer, 100000);
 	  }
 
 	  @Override
